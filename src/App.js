650adbf4325f80fbc8e2bed7c8362dbc6c30b578
Marshel Barbash <marshel13@gmail.com>
2019-04-15 23:54:04 -0400

    Added minor error handling and place API call in the backend

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-
-import Clarifai from 'clarifai';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '8ff8a4fcafcb4fc2b59017ad7b30df46'
-});
 
 const particlesOptions = {
   particles: {
@@ -28,25 +22,27 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  boxes: [],
+  route: 'signin',
+  isSignedIn : false,
+  user: {
+    id: '',
+    email: '',
+    name: '',
+    password: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      boxes: [],
-      route: 'signin',
-      isSignedIn : false,
-      user: {
-        id: '',
-        email: '',
-        name: '',
-        password: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -54,7 +50,7 @@ class App extends Component {
         id: data.id,
         email: data.email,
         name: data.name,
-        entries: 0,
+        entries: data.entries,
         joined: data.joined
       }});
   }
@@ -64,7 +60,6 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-
     return clarifaiFaces.map((face) => { 
       const id = face.id;
       const boundingBox = face.region_info.bounding_box;
@@ -87,29 +82,34 @@ class App extends Component {
 
   onSubmit = (event) => {
     this.setState({ imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: this.state.user.id })
+
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ input: this.state.input })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ id: this.state.user.id })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }));
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
-        }
-        this.displayFaceBoxes(this.calculateFaceLocation(response));
-      })
-      .catch(err => console.log(err)
-    );
+          .catch(console.log);
+      }
+      this.displayFaceBoxes(this.calculateFaceLocation(response));
+    })
+    .catch(err => console.log('Clarifai Error: ', err));
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false});
+      this.setState(initialState);
     }
     else if (route === 'home') {
       this.setState({ isSignedIn: true});
